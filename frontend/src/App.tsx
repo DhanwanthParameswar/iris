93fb4f26047f8f312aa464ac9642abe270d7964a
DhanwanthParameswar <im@dhanwanth.com>
2026-02-14 17:45:17 -0500

    Remove BrowserRouter from App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 // Import your existing pages
 import MainPage from "./pages/MainPage";
